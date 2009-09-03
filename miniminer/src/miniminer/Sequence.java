0b2f020960e473d901627423d65047bc8b228983
Ehsan Tabari <etabari@users.noreply.github.com>
2009-09-03 19:18:05 +0000

    Sequence class changed to work with new AminoAcid Enum

--- miniminer/src/miniminer/Sequence.java
+++ miniminer/src/miniminer/Sequence.java
@@ -1,30 +1,29 @@
 package miniminer;
 
+
+
 public class Sequence {
 	public String name;
-	private int[] data;
+	private AminoAcid[] data;
 
-	public final static String AminoAcidCodes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
-	public final static int Gap = AminoAcidCodes.indexOf('-');;
-
-	public Sequence(Sequence sequence) {
-		this.data = sequence.data.clone();
-		this.name = sequence.name;
+	public Sequence(Sequence Sequence) {
+		this.data = Sequence.data.clone();
+		this.name = Sequence.name;
 	}
 
-	public Sequence(String Name, int[] Data) {
+	public Sequence(String Name, AminoAcid[] Data) {
 		this.name = Name;
-		this.data = new int[Data.length];
+		this.data = new AminoAcid[Data.length];
 		for (int i = 0; i < Data.length; i++)
 			this.data[i] = Data[i];
 	}
 
 	public Sequence(String Name, String Data) {
-		this.name = Name;
-		this.data = new int[Data.length()];
-		for (int i = 0; i < Data.length(); i++)
-			data[i] = AminoAcidCodes.indexOf(Data.charAt(i));
 		
+		this.name = Name;
+		this.data = new AminoAcid[Data.length()];
+		for (int i = 0; i < Data.length(); i++)
+			data[i] = AminoAcid.valueOf(Data.charAt(i));
 	}
 
 	@Override
@@ -40,12 +39,12 @@ public class Sequence {
 		return data.length;
 	}
 
-	public int[] getData() {
+	public AminoAcid[] getData() {
 		return data;
 	}
 
-	public int[] getData(int start, int length) {
-		int[] result = new int[length];
+	public AminoAcid[] getData(int start, int length) {
+		AminoAcid[] result = new AminoAcid[length];
 		for (int i = 0; i < length; i++)
 			result[i] = data[start + i];
 
@@ -55,34 +54,29 @@ public class Sequence {
 	public String getDataString() {
 		StringBuilder rb = new StringBuilder(data.length + 1);
 		for (int i = 0; i < data.length; i++)
-			rb.append(AminoAcidCodes.charAt(data[i]));
+			rb.append(data[i].getLetter());
 		return new String(rb);
 	}
 
-	public int getBaseIndexAt(int i) {
+	public AminoAcid getBaseAt(int i) {
 		return data[i];
 	}
 
-	public char getBaseAt(int i) {
-		return AminoAcidCodes.charAt(data[i]);
+	public char getBaseCharAt(int i) {
+		return getBaseAt(i).getLetter();
 	}
 
-//	public static int getBaseIndex(char c) {
-//		return AminoAcidCodes.indexOf(c);
-//	}
-//
-	public void setBaseIndexAt(int i, int baseIndex) {
-		data[i] = baseIndex;// AminoAcidCodes.indexOf(c);
+
+	public void setBaseAt(int i, AminoAcid base) {
+		data[i] = base;// AminoAcidCodes.indexOf(c);
 	}
 
 	public void setBaseAt(int i, char base) {
-		// data = data.substring(0,i) + base + data.substring(i+1,data.length());
-		data[i] = AminoAcidCodes.indexOf(base);
+		data[i] = AminoAcid.valueOf(base);
 	}
 	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return getDataString();
 	}
 
